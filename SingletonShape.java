ab663ff522a2b9151bc8b0b16ba42881f528b5b1
ASUS <uddin.stud.kuet.ac.bd@gmail.com>
2024-01-25 01:59:58 +0600

    single

---
+++ SingletonShape.java
@@ -0,0 +1,23 @@
+public class SingletonShape {
+    private static final SingletonShape instance = new SingletonShape();
+
+    private SingletonShape() {
+      //ugvytv
+    }
+
+    public static SingletonShape getSingle() {
+        return instance;
+    }
+
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+        if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
